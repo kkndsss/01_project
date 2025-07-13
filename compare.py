821eea28cb135019f4e419fe9714d37fda8d3cde
Kwon YongJin <kkndss@naver.com>
2025-07-14 02:49:39 +0900

    주석 수정

--- compare.py
+++ compare.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import json
 
-# 파일 경로는 네 환경에 맞게 수정!
+# 파일 업로드
 upload_json=st.file_uploader("json 파일을 업로드하세요", type="json")
 
 if upload_json is not None :
@@ -9,7 +9,7 @@ if upload_json is not None :
 
 
 
-# 라벨(한자)만 순서대로 리스트로 추출
+# 한자만 추출
     correct_hanja_list = []
     for col in data["Image_Text_Coord"]:
         for char_info in col:
